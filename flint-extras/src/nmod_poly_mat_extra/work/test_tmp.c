3bd6cfb465e3d9999b4243c5d21912385e6d4ba0
Gilles Villard <gilles.villard@ens-lyon.fr>
2025-12-04 19:11:16 +0100

    test

--- flint-extras/src/nmod_poly_mat_extra/work/test_tmp.c
+++ flint-extras/src/nmod_poly_mat_extra/work/test_tmp.c
@@ -8,56 +8,11 @@
 #include <pml/nmod_poly_mat_io.h>
 //#include "../../nmod_poly_mat_extras/test/testing_collection.h"
 
-#include "nmod_poly_mat_dixon.h"
-#include "nmod_poly_mat_description.h"
-
-
-// Random poly_mat which is nonsingular for x=0
-void nmod_poly_mat_rand_origin(nmod_poly_mat_t A, flint_rand_t state, slong order) 
-{
-
-    nmod_mat_t T1,T2;
-    nmod_mat_init(T1, A->r, A->r, A->modulus);
-    nmod_mat_init(T2, A->r, A->r, A->modulus);
-
-    nmod_mat_randtril(T1,state,0);
-    nmod_mat_randtriu(T2,state,0);
-
-    nmod_mat_mul(T1,T1,T2);
-
-    nmod_poly_mat_t B;
-    nmod_poly_mat_init(B, A->r, A->r, A->modulus);
-
-    // nmod_poly_mat_set_nmod_mat(B,T1); // GV does not work, bug ? Sam 31 mai 2025 14:29:48 CEST
-
-    for (slong i = 0; i < A->r; i++)
-    {
-        for (slong j = 0; j < A->r; j++)
-        {
-            if (nmod_mat_entry(T1, i, j) == 0)
-                nmod_poly_zero(nmod_poly_mat_entry(B, i, j));
-            else
-            {
-                nmod_poly_set_coeff_ui(nmod_poly_mat_entry(B, i, j),0,nmod_mat_entry(T1, i, j));
-            }
-        }
-    }
-
-    nmod_poly_mat_rand(A, state, order-1);
-    nmod_poly_mat_shift_left(A,A,1);
-
-    nmod_poly_mat_add(A,A,B);
-
-    nmod_mat_clear(T1);
-    nmod_mat_clear(T2); 
-    nmod_poly_mat_clear(B); 
-}
-
 
 
 int main(int argc, char ** argv)
 {
-    printf("Usage: %s OR %s [nbits] [rdim] [cdim] [order] [delta] \n--\n", argv[0], argv[0]);
+    printf("Usage: %s OR %s [nbits] [rdim] [cdim] [degree] \n--\n", argv[0], argv[0]);
 
     // disable line buffering
     setbuf(stdout, NULL);
@@ -66,22 +21,35 @@ int main(int argc, char ** argv)
     slong rdim = atoi(argv[2]);
     slong cdim = atoi(argv[3]);
     slong order = atoi(argv[4]);
-    slong delta = atoi(argv[5]);
 
     flint_rand_t state;
     flint_rand_init(state);
     srand(time(NULL));
     flint_rand_set_seed(state, rand(), rand());
 
+
     slong prime = n_randprime(state, nbits, 1);
-    printf("Launching  with\n\tprime = %ld,\n\trdim = %ld,\n\tcdim = %ld,\
-            \n\torder = %ld, \n\tdelta = %ld...\n",prime,rdim,cdim,order,delta);
 
     nmod_poly_mat_t A;
     nmod_poly_mat_init(A, rdim, cdim, prime);
-    nmod_poly_mat_rand(A, state, order+1);
+
+    nmod_poly_mat_randtest_sparse(mat, state, order+1, 0.05);
+
 
     nmod_poly_mat_t N; // Not initialized 
+
+    printf("Launching  with\n\tprime = %ld,\n\trdim = %ld,\n\tcdim = %ld,\
+            \n\tdegree = %ld ...\n",prime,rdim,cdim,order);
+
+
+
+static inline void _test_collection_mat_sparse(nmod_poly_mat_t mat, slong deg, flint_rand_t state)
+{
+   
+}
+
+
+    
 
     slong nbnull;
 
